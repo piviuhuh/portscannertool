02ce7794cd95cf97bf399a9e5656edb61d77859d
Matia Pivirotto <matia.pivirotto@gmail.com>
2019-06-05 11:30:06 +0200

    Update portscanner.py

--- portscanner.py
+++ portscanner.py
@@ -10,15 +10,15 @@ host_scan = input("Which HOST do you want to scan?")
 first_port_scanner.scan(host_scan,"1-1024")
 
 for host in first_port_scanner.all_hosts():
-     print('Host : %s (%s)' % (host, nmScan[host].hostname()))
-     print('State : %s' % nmScan[host].state())
-     for proto in nmScan[host].all_protocols():
+     print('Host : %s (%s)' % (host, first_port_scanner[host].hostname()))
+     print('State : %s' % first_port_scanner[host].state())
+     for proto in first_port_scanner[host].all_protocols():
          print('----------')
          print('Protocol : %s' % proto)
  
-         lport = nmScan[host][proto].keys()
+         lport = first_port_scanner[host][proto].keys()
          lport.sort()
          for port in lport:
-             print ('port : %s\tstate : %s' % (port, nmScan[host][proto][port]['state'])
+             print ('port : %s\tstate : %s' % (port, first_port_scanner[host][proto][port]['state'])
     
 
